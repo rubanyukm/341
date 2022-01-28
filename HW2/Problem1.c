ff023f6d896df87d1674d6a86e057302a121aa28
Ruben Maidaniuc <50090403+rubanyukm@users.noreply.github.com>
2022-01-27 20:36:17 -0500

    V2

--- HW2/Problem1.c
+++ HW2/Problem1.c
@@ -5,7 +5,21 @@
 
 double compute_pi(int n) {
 
+    double total = 0;
+    int four = 4;
+    double infSum; 
+    for(int i = 1; i < n; ++i) {
+        if(i % 2 != 0) {
+           infSum = four * (1 - (1/n) + (1/n));
+        }
     
+        return infSum;
+    }
 
+}
+
+int main() {
+
+    printf("The desired value of pi is %f", compute_pi(4));
     return 0;
 }
