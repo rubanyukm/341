97f57efb9f2f9a6ce32049120731e8ead8031b5d
Ruben Maidaniuc <50090403+rubanyukm@users.noreply.github.com>
2022-01-29 13:48:44 -0500

    Final Version

--- HW2/Problem1.c
+++ HW2/Problem1.c
@@ -1,25 +1,38 @@
 
 //Problem 1 from HW2 written in C
+/* Write a function that computes the value of the mathematical constant π. The value of
+π can be approximately computed using the infinite sum 4(1 - 1/3 + 1/5 - 1/7
++ 1/9 - 1/11 + 1/13 - 1/15 + . . .) The function prototype is double
+compute_pi(int n). The function computes the value of π using the first n terms of
+the infinite sum and returns the value. */
 
-#include <stdio.h>;
+#include <stdio.h>
+#include <math.h>
 
 double compute_pi(int n) {
 
-    double total = 0;
-    int four = 4;
-    double infSum; 
-    for(int i = 1; i < n; ++i) {
-        if(i % 2 != 0) {
-           infSum = four * (1 - (1/n) + (1/n));
-        }
-    
-        return infSum;
+    long int i;
+    double sum=0.0, term, pi;
+ 
+
+    for(i = 0; i < n; i++) {
+        term = pow(-1, i) / (2*i+1);
+        sum += term;
     }
+
+    pi = 4 * sum;
+ 
+    return pi;
 
 }
 
+
 int main() {
 
-    printf("The desired value of pi is %f", compute_pi(4));
+    int computation;
+    printf("Enter an integer: ");
+    scanf("%d", &computation); 
+
+    printf("\nThe desired value of pi is: %.6lf", compute_pi(computation));
     return 0;
 }
